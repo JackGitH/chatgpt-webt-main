c9ff3917c74ba82ebd090b5bffa35c76d86c8153
zjtt <893126716@qq.com>
2023-04-14 16:38:28 +0800

    perf: build

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,45 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 import ElementPlus from 'unplugin-element-plus/vite'
+import viteCompression from 'vite-plugin-compression'
+import viteImagemin from 'vite-plugin-imagemin'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 function setupPlugins(env: ImportMetaEnv): PluginOption[] {
   return [
     vue(),
     ElementPlus(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+    viteCompression({
+      threshold: 600000, // 对大于 600kb 的文件进行压缩
+      deleteOriginFile: true,
+    }),
     env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
       injectRegister: 'auto',
       manifest: {
@@ -20,6 +54,7 @@ function setupPlugins(env: ImportMetaEnv): PluginOption[] {
         ],
       },
     }),
+    visualizer(),
   ]
 }
 
@@ -45,11 +80,58 @@ export default defineConfig((env) => {
         },
       },
     },
+    // build: {
+    //   reportCompressedSize: false,
+    //   sourcemap: false,
+    //   commonjsOptions: {
+    //     ignoreTryCatch: false,
+    //   },
+    // },
     build: {
-      reportCompressedSize: false,
-      sourcemap: false,
-      commonjsOptions: {
-        ignoreTryCatch: false,
+      assetsDir: 'static',
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: env.mode === 'production', // 默认为false
+          drop_debugger: env.mode !== 'development', // 默认为true
+        },
+      },
+      // sourcemap: mode !== "production",
+      rollupOptions: {
+        output: {
+          // 静态资源分类打包
+          chunkFileNames: 'static/js/[name]-[hash].js',
+          entryFileNames: 'static/js/[name]-[hash].js',
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+          manualChunks(id) {
+            if ((id.includes('vue') && !id.includes('.vue'))
+            || id.includes('vue-router'))
+              return 'vue'
+            if (id.includes('element-plus'))
+              return 'element'
+            if (id.includes('highlight.js'))
+              return 'highlight'
+
+            // 将 node_modules 中的代码单独打包成一个 JS 文件
+            if (id.includes('node_modules'))
+              return 'vendor'
+          },
+          // manualChunks(id) {
+          //   // 将 node_modules 中的代码单独打包成一个 JS 文件
+          //   if (id.includes('node_modules'))
+          //   return 'vendor'
+          // },
+          // manualChunks(id) {
+          //   //静态资源分拆打包
+          //   if (id.includes("node_modules")) {
+          //     return id
+          //       .toString()
+          //       .split("node_modules/")[1]
+          //       .split("/")[0]
+          //       .toString();
+          //   }
+          // },
+        },
       },
     },
   }
