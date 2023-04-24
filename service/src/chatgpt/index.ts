3d2ec0211f089f1d5d606d0af94a06c6d3ab6310
ghc <guofly123@qq.com>
2023-04-24 08:58:24 +0800

    feat: 新分支基础上修改为原来的调用模式

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -27,27 +27,21 @@ const timeoutMs: number = !isNaN(+process.env.TIMEOUT_MS) ? +process.env.TIMEOUT
 const disableDebug: boolean = process.env.OPENAI_API_DISABLE_DEBUG === 'true'
 
 let apiModel: ApiModel
-let model = 'gpt-3.5-turbo'
+const model = isNotEmptyString(process.env.OPENAI_API_MODEL) ? process.env.OPENAI_API_MODEL : 'gpt-3.5-turbo'
 
+if (!isNotEmptyString(process.env.OPENAI_API_KEY) && !isNotEmptyString(process.env.OPENAI_ACCESS_TOKEN))
+  throw new Error('Missing OPENAI_API_KEY or OPENAI_ACCESS_TOKEN environment variable')
 
-const api = createApi(process.env.OPENAI_API_KEY,0)
+let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
 
-function createApi(apiKey1,indexI) {
-  //const apiKey1 = "";
-  // if (!isNotEmptyString(process.env.OPENAI_API_KEY) && !isNotEmptyString(process.env[accessToken]))
-  // 	throw new Error('Missing OPENAI_API_KEY or OPENAI_ACCESS_TOKEN environment variable')
-
-  let api: ChatGPTAPI | ChatGPTUnofficialProxyAPI
-
+(async () => {
   // More Info: https://github.com/transitive-bullshit/chatgpt-api
 
-  if (isNotEmptyString(apiKey1)) {
+  if (isNotEmptyString(process.env.OPENAI_API_KEY)) {
     const OPENAI_API_BASE_URL = process.env.OPENAI_API_BASE_URL
-    const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL
-    model = isNotEmptyString(OPENAI_API_MODEL) ? OPENAI_API_MODEL : 'gpt-3.5-turbo'
 
     const options: ChatGPTAPIOptions = {
-      apiKey: apiKey1,
+      apiKey: process.env.OPENAI_API_KEY,
       completionParams: { model },
       debug: !disableDebug,
     }
@@ -74,135 +68,22 @@ function createApi(apiKey1,indexI) {
     apiModel = 'ChatGPTAPI'
   }
   else {
-    const accessToken = "";
-    const OPENAI_API_MODEL = process.env.OPENAI_API_MODEL
     const options: ChatGPTUnofficialProxyAPIOptions = {
-       accessToken: accessToken,
+      accessToken: process.env.OPENAI_ACCESS_TOKEN,
+      apiReverseProxyUrl: isNotEmptyString(process.env.API_REVERSE_PROXY) ? process.env.API_REVERSE_PROXY : 'https://bypass.churchless.tech/api/conversation',
+      model,
       debug: !disableDebug,
     }
-
-    if (isNotEmptyString(OPENAI_API_MODEL))
-      options.model = OPENAI_API_MODEL
-    var portn = Number(Number(8081)+Number(indexI))
-
-    options.apiReverseProxyUrl = isNotEmptyString(process.env.API_REVERSE_PROXY)
-      ? process.env.API_REVERSE_PROXY+portn+"/conversation"
-      : 'https://bypass.churchless.tech/api/conversation'
 
     setupProxy(options)
 
     api = new ChatGPTUnofficialProxyAPI({ ...options })
     apiModel = 'ChatGPTUnofficialProxyAPI'
   }
-  return api
-}
-
-let beginAmount = 1
-const apiMap = new Map()
-const useMap = new Map()
-const timeMap = new Map()
-let conIdArr = []
-
-
-const apiArr: (ChatGPTAPI | ChatGPTUnofficialProxyAPI)[] = []
-for (let i = 0; i < 1; i++) {
-
-  //const api = createApi(tokens[i])
-  //const api = createApi(process.env[`OPENAI_ACCESS_TOKEN${i}`],i)
-  //const api = createApi(process.env[`OPENAI_API_KEY${i}`])
-  const api = createApi("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",0)
-  apiArr.push(api)
-  apiMap.set(api, false)
-}
-
-
-//const aaa = setInterval(doClear, 86400000)
-
-function doClear() {
-  const lsArr = []
-  for (let i = 0; i < conIdArr.length; i++) {
-    const conIdNow = conIdArr[i]
-    const datenow = timeMap.get(conIdNow)
-    useMap.clear()
-    const NowDate = (new Date()).valueOf()
-    if ((NowDate - datenow) >= 172800000) {
-      timeMap.delete(conIdNow)
-      useMap.delete(conIdNow)
-    }
-    else {
-      lsArr.push(conIdNow)
-    }
-  }
-  conIdArr = lsArr
-}
-// console.log(JSON.stringify(apiArr))
-/* function findUseApi() {
-  const len: number = apiArr.length
-  for (let i = 0; i < len; i++) {
-    const flag = apiMap.get(apiArr[i])
-    if(!flag) {return apiArr[i]}
-  }
-  return false
-  //const willGet = beginAmount % baseAmount
-} */
-function findUseApi() {
-  const len: number = apiArr.length
-  // console.log("len:{}"+len)
-  // console.log("beginAmount-1:{}"+(beginAmount-1))
-  const willGet = (beginAmount - 1) % len
-  beginAmount += 1
-  console.log(new Date()+"willGet++++++:"+willGet)
-  // console.log("willGet:{}"+willGet)
-  return apiArr[willGet]
-}
+})()
 
 async function chatReplyProcess(options: RequestOptions) {
   const { message, lastContext, process, systemMessage, temperature, top_p } = options
-  // console.log("lastContext.conversationId:{}"+lastContext.conversationId)
-  // console.log("options.systemMessage:{}"+options.systemMessage)
-  // console.log("options.top_p:{}"+options.top_p)
-  // console.log("options.process:{}"+options.process)
-  // console.log("options.message:{}"+options.message)
-  //let api = null
-  //let iNew = true
-
-  // logger.info("lastContext.conversationId:{} message:{}",lastContext.conversationId,message)
-  // logger.info()
-
-  console.log(new Date()+`lastContext.conversationId:${lastContext.conversationId}__message:${message}`)
-  const params = new URLSearchParams()
-  params.append('messageId', lastContext.conversationId)
-  params.append('message', message)
-  try {
-    await fetch('http://8.218.226.69:9999/conversation/add', {
-      method: 'post',
-      body: params,
-    }).then()
-  }
-  catch (errornow: any) {
-    console.log(new Date()+'传输错误A')
-  }
-
-  if (lastContext.conversationId != '' && lastContext.conversationId != null && lastContext.conversationId != 'undefined') {
-   // console.log(new Date()+'old api')
-    //api = useMap.get(lastContext.conversationId)
-    /*if (api == 'undefined' || api == null || api == '')
-      api = findUseApi()
-
-    iNew = true*/
-  }
-  else {
-    //console.log(new Date()+'begin api')
-   // api = findUseApi()
-    //iNew = false
-  }
-  if(apiMap.get(api)){
-   // return sendResponse({ type: 'Fail', message: '限流，请3秒后再试或左上角新建会话开启新进程' })
-  }
-
-  // const flag = apiMap.get(api)
-  // if(flag) return sendResponse({ type: 'Fail', message: 'Service busy, wait 10 seconds and try again' })
-  //apiMap.set(api, true)
   try {
     let options: SendMessageOptions = { timeoutMs }
 
@@ -218,42 +99,18 @@ async function chatReplyProcess(options: RequestOptions) {
       else
         options = { ...lastContext }
     }
+
     const response = await api.sendMessage(message, {
       ...options,
       onProgress: (partialResponse) => {
         process?.(partialResponse)
       },
     })
-    //apiMap.set(api, false)
 
-    // console.log("response.conversationId:{}"+response.conversationId)
-    // console.log("response:{}"+response.conversationId)
-    // if(response.conversationId=="" || response.conversationId==null || response.conversationId=="undefined"){
-   // useMap.set(response.conversationId, api)
-    //timeMap.set(response.conversationId, (new Date()).valueOf())
-    //if (iNew)
-     // conIdArr.push(response.conversationId)
-
-    // }
-    // console.log("responseaaa:"+JSON.parse(response).get("text"))
     return sendResponse({ type: 'Success', data: response })
   }
   catch (error: any) {
-    apiMap.set(api, false)
-    console.log(new Date()+`errora:${error}`)
-    /*		if(error.contains("Conversation not found")){
-      return sendResponse({ type: 'Fail', message: "会话过期，请新建对话" })
-    } */
     const code = error.statusCode
-    if (code == ('404'))
-      return sendResponse({ type: 'Fail', message: '会话过期，请左上角新建对话' })
-
-    if (code == ('429'))
-      return sendResponse({ type: 'Fail', message: 'Too many requests in 1 hour. Try again later' })
-
-    if (code == ('400'))
-      return sendResponse({ type: 'Fail', message: '代理服务限流，请5秒后再试或左上角新建会话开启新进程' })
-
     global.console.log(error)
     if (Reflect.has(ErrorCodeMessage, code))
       return sendResponse({ type: 'Fail', message: ErrorCodeMessage[code] })
